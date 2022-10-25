aab953ca6570504707dfad645d1f1881e90ce22e
badutyah <88601771+badutyah@users.noreply.github.com>
2022-10-25 09:08:42 +0700

    config.js

--- config.js
+++ config.js
@@ -17,12 +17,12 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE DANIXD'
+global.ownername = 'TITID'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['6281251549899','6281251549899','6281251549899']
+global.owner = ['6285975406775','6285975406775','6285975406775']
 global.premium = ['6281251549899']
 global.packname = 'The Jo Bot'
 global.author = 'WhatsApp Bot'
